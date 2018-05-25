3a4ab5544fe11dedada573dc1b8ce802d9d66f91
brian <@laskow26>
2018-05-24 20:50:52 -0400

    adjusting install

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 PWD=$(pwd)
 
-mkdir /usr/local/scan
+mkdir /usr/local/scan 2> /dev/null
 cp -av $PWD/blazescan /usr/local/scan/
-ln -s /usr/local/scan/blazescan /usr/local/bin/blazescan
+ln -s /usr/local/scan/blazescan /usr/local/bin/blazescan 2> /dev/null
 
 echo -e "install complete"
 
