bf794424487a6cf5dc04ec613ba0cee57df21e2e
brian <@laskow26>
2018-05-30 18:59:35 -0400

    install checks for wpcli and comments

--- install.sh
+++ install.sh
@@ -91,5 +91,5 @@ mkdir /usr/local/scan 2> /dev/null
 cp -av $PWD/blazescan /usr/local/scan/
 ln -s /usr/local/scan/blazescan /usr/local/bin/blazescan 2> /dev/null
 
-echo -e "install complete"
+echo -e "\nInstall complete\n Good Hunting\n"
 
