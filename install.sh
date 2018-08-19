f08732e4cd798bb95fce88cebe611fa7884fba71
brian <@laskow26>
2018-08-19 15:32:18 -0400

    man pages

--- install.sh
+++ install.sh
@@ -141,13 +141,13 @@ if [[ -e /usr/local/scan/blazescand.conf ]];then
 	cp -av $PWD/blazescan /usr/local/scan/
 	ln -s /usr/local/scan/blazescan /usr/local/bin/blazescan 2> /dev/null
 	cp -av $PWD/blazescan-man /usr/local/man/man1/blazescan.1
-	gzip /usr/local/man/man1/blazescan.1
+	gzip -f /usr/local/man/man1/blazescan.1
 else
 	cp -av $PWD/blazescand.conf /usr/local/scan/
 	cp -av $PWD/blazescan /usr/local/scan/
 	ln -s /usr/local/scan/blazescan /usr/local/bin/blazescan 2> /dev/null
 	cp -av $PWD/blazescan-man /usr/local/man/man1/blazescan.1
-	gzip /usr/local/man/man1/blazescan.1
+	gzip -f /usr/local/man/man1/blazescan.1
 fi
 
 echo -e "$green\n Install complete\n Good Hunting\n $whi"
